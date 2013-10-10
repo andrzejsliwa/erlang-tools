a55131bf852fea84f758af350d87a8b05dba3ee7
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2013-10-10 21:04:58 +0200

    fix erl function

--- erlang-tools.bash
+++ erlang-tools.bash
@@ -8,5 +8,5 @@ function erlang {
 
 function erl {
     local node_name=$(basename $PWD)
-    command erl -config ~/erl_hist.config -name $node_name@127.0.0.1 -pa $PWD/ebin -pa $PWD/apps/*/ebin -pa $PWD/deps/*/ebin
+    command erl -config ~/erl_hist.config -name $node_name@127.0.0.1 -pa $PWD/ebin -pa $PWD/apps/*/ebin -pa $PWD/deps/*/ebin $@
 }
